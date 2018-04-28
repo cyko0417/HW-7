650297a241a4a65987b4f9ae9eda025c32966051
cyko <cyko@yahoo-inc.com>
2018-04-28 09:50:08 +0800

    Initial Commit

---
+++ HW-7/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HW-7
+//
+//  Created by  Chih-Yu Ko on 2018/4/28.
+//  Copyright © 2018年 Oath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HW-7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HW-7
+//
+//  Created by  Chih-Yu Ko on 2018/4/28.
+//  Copyright © 2018年 Oath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
